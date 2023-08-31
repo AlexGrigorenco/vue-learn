b4224b71bea0429629c41ce813c78ef80c6f8e68
AlexGrigorenco <alpsnhandy@gmail.com>
2023-08-31 21:53:42 +0300

    create App component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="elem">
+        <div>
+            Количество лайков: <strong>{{likes}}</strong>
+        </div>
+    </div>
+    <div class="elem">
+        <div>
+            Количество дизлайков: <strong>{{dislikes}}</strong>
+        </div>
+    </div>
+    <div class="bttn-wrapper">
+        <div>
+            <button v-on:click="addLike">Like</button>
+        </div>
+        <div>
+            <button @click="addDislike">Dislike</button>
+        </div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    data() {
+        return {
+            likes: 10,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes +=1;
+        },
+        addDislike(){
+            this.dislikes +=1;
+        }
+    }
 }
+</script>
+<style scope>
+
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    .elem{
+        padding: 10px;
+    }
+    .bttn-wrapper{
+        display: flex;
+        gap: 10px;
+        padding: 10px;
+    }
+    button{
+        background: aqua;
+        border: 1px solid aqua;
+        padding: 10px 20px;
+        border-radius: 8px;
+        font-weight: bold;
+    }
+    button:hover{
+        transform: translateY(2px) scale(.97);
+        box-shadow: 2px 2px 4px #a8a4a4;
+    }
+    button:active{
+        opacity: .5;
+        transform: scale(.9);
+    }
 </style>
