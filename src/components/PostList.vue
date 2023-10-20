0de86d16d598393bed123847373feb542bae030e
AlexGrigorenco <alpsnhandy@gmail.com>
2023-10-20 03:12:13 +0300

    list-leave-to brocked

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,60 +1,77 @@
 <template>
-    <div class="post-wrapper">
-        <h3>Список Постов</h3>
-         <post-item 
-         @openPostInModal="openPostInModal"
-         v-for="post in posts"
-         :key="post.id"
-         :post="post"
-         @removePost="$emit('removePost', post)"
-         />
-    </div>
-    <my-modal
-    v-model:show="modalVisible">
-        <post-item 
-        @removePost="removePostInModal"
-        :post="individualPost"/>
-    </my-modal>
+  <div class="post-wrapper">
+    <h3>Список Постов</h3>
+    <transition-group name="list">
+      <post-item
+        @openPostInModal="openPostInModal"
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @removePost="$emit('removePost', post)"
+      />
+    </transition-group>
+  </div>
+  <my-modal v-model:show="modalVisible">
+    <post-item @removePost="removePostInModal" :post="individualPost" />
+  </my-modal>
 </template>
 
 <script>
-    import PostItem from '@/components/PostItem'
-    export default {
-        components: {
-            PostItem,
-        },
+import PostItem from "@/components/PostItem";
+export default {
+  components: {
+    PostItem,
+  },
 
-        props: {
-            posts: {
-                type: Array,
-                required: true
-            }
-        },
-        data(){
-            return {
-                individualPost: {},
-                modalVisible: false,
-            }
-        },
-        methods: {
-            openPostInModal(postData){
-                this.individualPost = postData;
-                this.modalVisible = true;
-            },
-            removePostInModal(){
-                this.$emit('removePost', this.individualPost);
-                this.modalVisible = false;
-            }
-        }
-    }
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      individualPost: {},
+      modalVisible: false,
+    };
+  },
+  methods: {
+    openPostInModal(postData) {
+      this.individualPost = postData;
+      this.modalVisible = true;
+    },
+    removePostInModal() {
+      this.$emit("removePost", this.individualPost);
+      this.modalVisible = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
+.post-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
 
-.post-wrapper{
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
+.list-item {
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+.list-enter-from{
+    opacity: 0;
+    transform: translateX(100%);
+}
+.list-leave-to {
+    opacity: 0;
+  transform: scale(0);
+}
+
+.list-list-move {
+  transition: transform 0.8s ease;
 }
 
 
