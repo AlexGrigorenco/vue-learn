5e077e20bd1482988c283505dd0b0c5b5e65bd7f
AlexGrigorenco <alpsnhandy@gmail.com>
2023-12-10 19:24:25 +0200

    added gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "/vue-learn/",
   transpileDependencies: true,
   // configureWebpack: {
   //   resolve: {
